0d67fde59e71f4d023480299ece70b4a55eeca2f
Nasim <mdnasimhosen333@gmail.com>
2025-04-28 11:45:35 +0600

    solved problem 3

---
+++ Khana_Pina_1.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+int main()
+{
+    int m;
+    scanf("%d", &m);
+    if (m >= 1000)
+    {
+        printf("Three Kacchi\n");
+    }
+    else if (m >= 500)
+    {
+        printf("One Large Pizza\n");
+    }
+    else if (m < 500 && m >= 250)
+    {
+        printf("Three Small Burger\n");
+    }
+    else if (m >= 100)
+    {
+        printf("Three Fuchka\n");
+    }
+    else if (m < 100)
+    {
+        printf("Nothing\n");
+    }
+    return 0;
+}
