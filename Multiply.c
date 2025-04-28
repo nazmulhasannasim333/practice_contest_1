68a6c3f8d54cffcf16e859f2c2a443635a9a67a3
Nasim <mdnasimhosen333@gmail.com>
2025-04-28 11:36:25 +0600

    solved problem 2

---
+++ Multiply.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main()
+{
+    long long int a, b;
+    scanf("%lld %lld", &a, &b);
+    long long int multiply = a * b;
+    printf("%lld\n", multiply);
+    return 0;
+}
