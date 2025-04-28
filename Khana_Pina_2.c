5a9cd1376d6d92eda5a89e555121dacb19252c25
Nasim <mdnasimhosen333@gmail.com>
2025-04-28 11:48:25 +0600

    solved problem 4

---
+++ Khana_Pina_2.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main()
+{
+    int m;
+    scanf("%d", &m);
+    if (m % 3 == 0)
+    {
+        printf("Yes\n");
+    }
+    else
+    {
+        printf("No\n");
+    }
+    return 0;
+}
